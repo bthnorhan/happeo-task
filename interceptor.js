b6fbeffbac2a0da04cca9a0cd8176d39b9333179
Batuhan Orhan <bthnorhan00@gmail.com>
2021-05-22 02:43:16 +0300

    Task done according cases

--- interceptor.js
+++ interceptor.js
@@ -1,8 +1,32 @@
 function batchInterceptor(instance) {
 	instance.interceptors.request.use(
 		(request) => {
-			// Add your code here
+			// If params.ids is an array, join them with comma (s),
+			// don't change anything.
+			const ids = Array.isArray(request.params.ids)
+				? request.params.ids.join(',')
+				: request.params.ids;
+
+			// Set new parameters
+			request.params.ids = ids;
 			return request;
+		},
+		(error) => Promise.reject(error),
+	);
+
+	instance.interceptors.response.use(
+		(response) => {
+			// Parsing requested ids
+			const paramIds = response.config.params.ids.split(',');
+			// Check ids in response
+			const found = response.data.items.some((item) => paramIds.includes(item.id));
+			if (!found) {
+				// Reject the request if response didn't include ids.
+				return Promise.reject(`${response.config.params.ids} is missing from te response`);
+			}
+
+			// Return object as wanted
+			return response.data.items;
 		},
 		(error) => Promise.reject(error),
 	);
